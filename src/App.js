e9eef24c50840b1af1b76b3b5eb925162987ec1d
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2024-01-15 21:46:33 +0600

    Adds cheat code tooltip function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import "./App.css"
 
+import React from "react"
+
+import * as cheats from "./cheats"
+
+function displayCheatMessage(message) {}
+
+function CheatMessage({ message = "Cheat Activated" }) {
+  return <div className="cheat_message">{message}</div>
+}
+
 function App() {
+  const [tooltipShown, setTooltipShown] = React.useState(false)
+  const [tooltipMessage, setTooltipMessage] = React.useState("")
+
   function handleSearchSubmit(event) {
     event.preventDefault()
     const search = event.target.elements.search.value
 
     if (!search) return
 
-    alert(search)
+    handleCheatCode(search)
   }
+
+  function handleCheatCode(cheat) {
+    if (cheat.toUpperCase() === "LEAVEMEALONE") {
+      setTooltipShown(true)
+      setTooltipMessage("Wanted Level Cheat")
+    }
+  }
+
+  React.useEffect(() => {
+    if (!tooltipShown) return
+
+    setTimeout(() => {
+      setTooltipShown(false)
+    }, 3000)
+  }, [tooltipShown])
 
   return (
     <div className="App">
+      {tooltipShown ? <CheatMessage message={tooltipMessage} /> : null}
       <form onSubmit={handleSearchSubmit}>
         <div>
-          <input type="text" id="search" />
+          <input type="text" id="search" autoFocus={true} />
         </div>
         <button type="submit">Enter</button>
       </form>
---
+++ src/cheats.js
@@ -0,0 +1,23 @@
+function activateWeaponCheat() {
+  alert("Activating the weapon cheat code")
+}
+
+// Spawn Hearse - THELASTRIDE
+// Spawn Hotring Racer - GETTHEREVERYFASTINDEED
+// Spawn Hunter - AMERICAHELICOPTER
+// Spawn Limo - ROCKANDROLLCAR
+// Spawn Sabre Turbo - GETTHEREFAST
+// Spawn Tank - PANZER
+// Spawn Trashmaster - RUBBISHCAR
+// Speed Up Gameplay - ONSPEED
+// Suicide - ICANTTAKEITANYMORE
+// Sunny Weather - ALOVELYDAY
+// Very Cloudy Weather - ABITDRIEG
+// export const THUGSTOOLS = activateWeaponCheat()
+
+export const PROFESSIONALTOOLS = function activateWeaponCheat() {
+  alert("PROFESSIONAL TOOLS")
+}
+export const NUTTERTOOLS = function activateWeaponCheat() {
+  alert("NUTTER TOOLS")
+}
